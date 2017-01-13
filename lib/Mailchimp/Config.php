aec7c5084c43ef383b8a9f9da1e1514d1e77cc60
David Vandemaele <david@tigron.be>
2017-01-13 10:47:36 +0100

    Add configuration

---
+++ lib/Mailchimp/Config.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Config class
+ * Configuration for Maimchimp
+ *
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+namespace Mailchimp;
+
+class Config {
+
+	/**
+	 * Api endpoint
+	 *
+	 * @access public
+	 * @var string $api_endpoint
+	 */
+	public static $api_endpoint = null;
+
+	/**
+	 * Api key
+	 *
+	 * @access public
+	 * @var string $api_key
+	 */
+	public static $api_key = null;
+
+	/**
+	 * Export url
+	 *
+	 * @access public
+	 * @var string $export_url
+	 */
+	public static $export_url = null;
+
+}
